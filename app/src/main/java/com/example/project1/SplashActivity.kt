e62446c3a12b415bac2b826d08e44891369080e4
dhruv dhola <dhruvdhola67@gmail.com>
2024-03-16 01:50:22 -0400

    Add Firebase Login and Register Features Add code in splash activity if user already login or not Working On Candidate Details Activity Design

--- app/src/main/java/com/example/project1/SplashActivity.kt
+++ app/src/main/java/com/example/project1/SplashActivity.kt
@@ -1,11 +1,61 @@
 package com.example.project1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
 
 class SplashActivity : AppCompatActivity() {
+
+    private lateinit var joinBtn: Button
+    private lateinit var loginBtn: Button
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        auth = Firebase.auth
+        bindwidgets()
     }
+
+    override fun onStart() {
+        super.onStart()
+        val currentUser = auth.currentUser
+        if (currentUser != null) {
+            val i = Intent(this, MainActivity::class.java)
+            startActivity(i)
+            finish()
+        }
+    }
+
+    fun bindwidgets() {
+
+        joinBtn = findViewById(R.id.join_btn)
+        loginBtn = findViewById(R.id.signin_btn)
+
+        routeLogin()
+        routeSignUp()
+    }
+
+    fun routeSignUp() {
+        joinBtn.setOnClickListener {
+            val intent = Intent(this, SignupActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+
+    fun routeLogin() {
+        loginBtn.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+
+
 }
